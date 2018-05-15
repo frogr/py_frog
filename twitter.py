27326f18d63379a9430f47518aa0af93e72a2ad0
frogr <austindanielfrench@gmail.com>
2018-05-15 00:04:15 -0700

    i cant believe this works

--- twitter.py
+++ twitter.py
@@ -1,15 +1,35 @@
-import twitter
+from twython import Twython, TwythonStreamer
 from py_frog import configuration
-t_api = twitter.Api(
-    consumer_key=configuration.api_consumer_t,
-    consumer_secret=configuration.api_consumer_s,
-    access_token_key=configuration.api_access_t,
-    access_token_secret=configuration.api.access_s
+import hashlib
+
+uniqueKey = hashlib.md5().hexdigest()
+
+twitter = Twython(
+    configuration.api_consumer_t,
+    configuration.api_consumer_s,
+    configuration.api_access_t,
+    configuration.api_access_s
 )
 
-status = t_api.postUpdate('testing123')
-print(status.text)
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, tweet):
+        if 'text' in tweet:
+            if tweet['user']['id'] != 996274174959075328:
+                twitter.update_status(status="key: %s // %s" % (uniqueKey, tweet['text']))
+                print('tweet posted successfully')
+
+    def on_error(self, status_code, data):
+        print(status_code)
+        stream.statuses.filter(track='lele')
 
 
+stream = MyStreamer(
+    configuration.api_consumer_t,
+    configuration.api_consumer_s,
+    configuration.api_access_t,
+    configuration.api_access_s
+)
 
+stream.statuses.filter(track='lele')
 
