66167b3e7886b23d413b1b7dbe38d90971629f3a
frogr <austindanielfrench@gmail.com>
2018-05-15 07:27:28 -0700

    changes to make prettier

--- twitter.py
+++ twitter.py
@@ -1,6 +1,7 @@
 from twython import Twython, TwythonStreamer
 from py_frog import configuration
 import hashlib
+import time
 
 uniqueKey = hashlib.md5().hexdigest()
 
@@ -15,9 +16,10 @@ twitter = Twython(
 class MyStreamer(TwythonStreamer):
     def on_success(self, tweet):
         if 'text' in tweet:
-            if tweet['user']['id'] != 996274174959075328:
+            if tweet['user']['id'] != configuration.twitter_userID:
                 twitter.update_status(status="key: %s // %s" % (uniqueKey, tweet['text']))
                 print('tweet posted successfully')
+                time.sleep(60)
 
     def on_error(self, status_code, data):
         print(status_code)
